02869c07aa20b8d6ebdbede1b52d176d95a77065
Regine Mones <reginemones23@gmail.com>
2024-05-19 16:57:22 +0800

    signin page

--- resources/views/members/pages/Auth/signin.blade.php
+++ resources/views/members/pages/Auth/signin.blade.php
@@ -2,22 +2,26 @@
     {{ 'Sign in' }}
 @endsection
 
-<!--begin::Root-->
 <div class="d-flex flex-column flex-root" id="kt_app_root">
     <!--begin::Authentication - Sign-in -->
-    <div class="d-flex flex-column flex-lg-row flex-column-fluid bg-gray-200">
+    <div class="d-flex flex-column flex-lg-row flex-column-fluid" style="background-image: url('assets/media/public/general_bg.png'); background-size: cover; background-position: center">
+        <div class="position-absolute top-0 start-0 p-3">
+            <a href="" class="btn btn-dark btn-sm">
+                <i class="bi bi-arrow-left"></i>
+            </a>
+        </div>    
         <!--begin::Body-->
-        <div class="d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1">
+        <div class="d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1" style="background: linear-gradient(180deg, rgba(0,0,0,0.1966911764705882) 43%, rgba(0,0,0,0.2) 93%);">
             <!--begin::Form-->
             <div class="d-flex flex-center flex-column flex-lg-row-fluid">
                 <!--begin::Wrapper-->
-                <div class="w-lg-500px p-10">
+                <div class="card w-lg-400px p-10 shadow">
                     <!--begin::Form-->
                     <form wire:submit.prevent="login" class="form w-100 fv-plugins-bootstrap5 fv-plugins-framework"
                         novalidate="novalidate" id="kt_sign_in_form" data-kt-redirect-url="/metronic8/demo51/index.html"
                         action="#">
                         <!--begin::Heading-->
-                        <div class="text-center mb-11">
+                        <div class="text-start mb-11">
                             <!--begin::Title-->
                             <h1 class="text-gray-900 fw-bolder mb-3">
                                 Sign In
@@ -26,7 +30,7 @@
 
                             <!--begin::Subtitle-->
                             <div class="text-gray-500 fw-semibold fs-6">
-                                LifeLink
+                                Enter your Log In details.
                             </div>
                             <!--end::Subtitle--->
                         </div>
@@ -74,7 +78,7 @@
 
                             <!--begin::Link-->
                             <a href="/metronic8/demo51/authentication/layouts/corporate/reset-password.html"
-                                class="link-primary">
+                                class="link-dark">
                                 Forgot Password ?
                             </a>
                             <!--end::Link-->
@@ -83,7 +87,7 @@
 
                         <!--begin::Submit button-->
                         <div class="d-grid mb-10">
-                            <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+                            <button type="submit" id="kt_sign_in_submit" class="btn btn-dark">
 
                                 <!--begin::Indicator label-->
                                 <span class="indicator-label">
@@ -104,7 +108,7 @@
                             Not Registered yet?
 
                             <a href="{{ route('members.signup-1') }}"
-                                class="link-primary">
+                                class="link-dark">
                                 Sign up
                             </a>
                         </div>
@@ -117,38 +121,6 @@
             <!--end::Form-->
         </div>
         <!--end::Body-->
-        <!--begin::Aside-->
-        <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
-            style="background-image: url(assets/media/misc/auth-bg-1.jpg)">
-            <!--begin::Content-->
-            <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
-                <!--begin::Logo-->
-                <a href="../../demo36/dist/index.html" class="mb-0 mb-lg-12">
-                    <img alt="Logo" src="assets/media/logos/Lifelink-logo.png" class="h-60px h-lg-75px" />
-                </a>
-                <!--end::Logo-->
-                <!--begin::Image-->
-                <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20"
-                    src="assets/media/misc/auth-screens.png" alt="" />
-                <!--end::Image-->
-                <!--begin::Title-->
-                <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">Donate Blood, Save Lives</h1>
-                <!--end::Title-->
-                <!--begin::Text-->
-                <!--begin::Text-->
-                <div class="d-none d-lg-block text-white fs-base text-center">Blood donation is a voluntary act of
-                    donating blood or plasma to someone in need. It is a common need
-                    <br />for many medical treatments and emergencies.
-                </div>
-                <!--end::Text-->
-
-                <!--end::Text-->
-
-            </div>
-            <!--end::Content-->
-        </div>
-        <!--end::Aside-->
     </div>
     <!--end::Authentication - Sign-in-->
 </div>
-<!--end::Root-->
